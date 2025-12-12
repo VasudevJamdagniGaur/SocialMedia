ceb6089556ad503589ffc1bdcc4c572a8aa6b1be
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2025-12-13 05:12:08 +0530

    Enhance BottomNavigation icon opacity for better visibility in dark mode

--- src/components/BottomNavigation.js
+++ src/components/BottomNavigation.js
@@ -44,6 +44,9 @@ export default function BottomNavigation() {
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            style={{
+              opacity: isHomeActive ? 1 : (isDarkMode ? 0.7 : 0.6)
+            }}
           >
             <path
               d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
@@ -74,6 +77,9 @@ export default function BottomNavigation() {
             viewBox="0 0 24 24"
             fill="none"
             xmlns="http://www.w3.org/2000/svg"
+            style={{
+              opacity: isWellbeingActive ? 1 : (isDarkMode ? 0.7 : 0.6)
+            }}
           >
             <path
               d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"
