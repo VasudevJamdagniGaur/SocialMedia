094cf9c3be20a68c31cfba5e516beb2fe5931a03
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2026-03-11 18:17:33 +0530

    Update version code and name in build.gradle.kts for app release
    - Incremented versionCode to 7 and versionName to "1.0.6" to reflect the latest app release.
    - Ensured alignment with the project's versioning strategy for better tracking of updates.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "therapist.deite.app"
         minSdk = rootProject.ext["minSdkVersion"] as Int
         targetSdk = rootProject.ext["targetSdkVersion"] as Int
-        versionCode = 6
-        versionName = "1.0.5"
+        versionCode = 7
+        versionName = "1.0.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
         aaptOptions {
