3d17fcf913c27bec102315443745a7e948c8e1e0
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2026-03-13 23:35:06 +0530

    Update app name in configuration and string resources for consistency
    - Changed the app name from 'DeTea : AI Journel' and 'DeTea : AI Therapist' to 'DeTea' in both capacitor.config.ts and strings.xml files for a unified branding approach.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'therapist.deite.app',
-  appName: 'DeTea : AI Journel',
+  appName: 'DeTea',
   webDir: 'build',
   plugins: {
     FirebaseAuthentication: {
