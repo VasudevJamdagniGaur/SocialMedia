ed90b3912f2aa31bd07d4b897eec05e5e1aea135
VasudevJamdagniGaur <vasudevjamdagnigaur@gmail.com>
2026-05-25 12:45:20 +0530

    Update versionCode and versionName in build.gradle.kts for app release to 16 and 1.0.15 respectively

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "therapist.deite.app"
         minSdk = rootProject.ext["minSdkVersion"] as Int
         targetSdk = rootProject.ext["targetSdkVersion"] as Int
-        versionCode = 15
-        versionName = "1.0.14"
+        versionCode = 16
+        versionName = "1.0.15"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
         aaptOptions {
